bc163f740d8888775df81f1a5d9a21efa995bf9a
HananeZlitni <hananzlitni@hotmail.com>
2017-11-12 01:14:24 +0300

    completed screen 1.. only search left

--- app/src/main/java/com/example/hanan/usrealestate/MainActivity.java
+++ app/src/main/java/com/example/hanan/usrealestate/MainActivity.java
@@ -8,8 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import android.graphics.Color;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import java.util.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,14 +37,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Spinner spinner = (Spinner) findViewById(R.id.us_states);
+        final Spinner spinner = (Spinner) findViewById(R.id.stateField);
+
+        String[] usStates = new String[] {
+        "State","CA","NY","AL","AK","AZ","AR","CO","CT","DE","FL",
+                "GA","HI","ID","IL","IN","IA","KS","KY","LA","ME",
+                "MD","MA","MI","MN","MS","MO","MT","NE","NV","NH",
+                "NJ","NM","NC","ND","OH","OK","OR","PA","RI","SC",
+                "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY",
+        };
+
+        final List<String> usStatesList = new ArrayList<>(Arrays.asList(usStates));
+
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.states_array, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, usStatesList) {
+
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        Button clearBtn = (Button)findViewById(R.id.clear);
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.addressField)).setText("");
+                ((TextView)findViewById(R.id.cityField)).setText("");
+                ((Spinner)findViewById(R.id.stateField)).setSelection(0);
+            }
+        });
+
     }
 
     @Override
