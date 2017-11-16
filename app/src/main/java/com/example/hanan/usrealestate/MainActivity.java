c24190d48d5f234802da74a01325384633b97833
waad <435202715@student.ksu.edu.sa>
2017-11-17 00:31:57 +0300

    Merge branch 'master' of https://github.com/HananeZlitni/USRealEstate
    # Conflicts:
    #	app/src/main/java/com/example/hanan/usrealestate/MainActivity.java

--- app/src/main/java/com/example/hanan/usrealestate/MainActivity.java
+++ app/src/main/java/com/example/hanan/usrealestate/MainActivity.java
@@ -46,10 +46,33 @@ import java.net.URL;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.annotation.SuppressLint;
+
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import static android.graphics.Color.RED;
 
  import android.content.Context;
 public class MainActivity extends AppCompatActivity {
 
+    final String[] usStates = new String[] {
+            "State                      *", "CA","NY","AL","AK","AZ","AR","CO","CT","DE","FL",
+            "GA","HI","ID","IL","IN","IA","KS","KY","LA","ME",
+            "MD","MA","MI","MN","MS","MO","MT","NE","NV","NH",
+            "NJ","NM","NC","ND","OH","OK","OR","PA","RI","SC",
+            "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY",
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,38 +80,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //********** SET TEXT FIELDS ************
+        setTextFields();
 
+        //********** SPINNER ************
         final Spinner spinner = (Spinner) findViewById(R.id.stateField);
 
-        String[] usStates = new String[]{
-                "State", "CA", "NY", "AL", "AK", "AZ", "AR", "CO", "CT", "DE", "FL",
-                "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME",
-                "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH",
-                "NJ", "NM", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-                "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY",
-        };
-
-        final List<String> usStatesList = new ArrayList<>(Arrays.asList(usStates));
-
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, usStatesList) {
+        final List<String> stateArr = new ArrayList<>(Arrays.asList(usStates));
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.spinner_item,stateArr){
 
             @Override
-            public boolean isEnabled(int position) {
-                if (position == 0) {
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
                     // Disable the first item from Spinner
                     // First item will be use for hint
                     return false;
-                } else {
+                }
+                else
+                {
                     return true;
                 }
             }
@@ -98,20 +109,19 @@ public class MainActivity extends AppCompatActivity {
                                         ViewGroup parent) {
                 View view = super.getDropDownView(position, convertView, parent);
                 TextView tv = (TextView) view;
-                if (position == 0) {
+                if(position == 0){
                     // Set the hint text color gray
                     tv.setTextColor(Color.GRAY);
-                } else {
+                }
+                else {
                     tv.setTextColor(Color.BLACK);
                 }
                 return view;
             }
         };
 
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(spinnerArrayAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -119,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                 String selectedItemText = (String) parent.getItemAtPosition(position);
                 // If user change the default selection
                 // First item is disable and it is used for hint
-                if (position > 0) {
+                if(position > 0){
                     // Notify the selected item text
                     Toast.makeText
                             (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
@@ -129,33 +139,71 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
-        Button clearBtn = (Button) findViewById(R.id.clear);
+        //********** CLEAR BUTTON ************
+        Button clearBtn = (Button)findViewById(R.id.clear);
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView addressField = (TextView) findViewById(R.id.addressField);
-                TextView cityField = (TextView) findViewById(R.id.cityField);
-                Spinner stateField = (Spinner) findViewById(R.id.stateField);
+                TextView addressField = (TextView)findViewById(R.id.addressField);
+                TextView cityField =(TextView)findViewById(R.id.cityField);
+                Spinner stateField = (Spinner)findViewById(R.id.stateField);
                 addressField.setText("");
                 cityField.setText("");
                 stateField.setSelection(0);
             }
         });
 
-        /*Button searchBtn = (Button)findViewById(R.id.search);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
+        //********** SCREEN 1 TABLE ************
+        final TableRow tableRow = (TableRow)findViewById(R.id.table1row);
+        final TableLayout table1 = (TableLayout)findViewById(R.id.table1);
+        tableRow.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ClickableViewAccessibility")
             @Override
             public void onClick(View v) {
-                HTTPRequestClass httpRequest = new HTTPRequestClass();
-                httpRequest.execute();
-                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                tableRow.setOnTouchListener(new SwipeDismissTouchListener(
+                        tableRow,
+                        null,
+                        new SwipeDismissTouchListener.DismissCallbacks() {
+                            @Override
+                            public boolean canDismiss(Object token) {
+                                return true;
+                            }
 
-                startActivity(intent);
+                            @Override
+                            public void onDismiss(View view, Object token) {
+                                deleteAlert("Are You Sure You Want to Delete this Property?");
+                            }
+
+                            @Override
+                            public boolean performClick() {
+                                return true;
+                            }
+
+                            public void deleteAlert(String msg) {
+                                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                                alertDialog.setMessage(msg);
+                                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "CANCEL",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                table1.removeView(tableRow);
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                alertDialog.show();
+                            }
+                        }));
             }
-        });*/
+        });
     }
 
     @Override
@@ -180,18 +228,67 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void goToActivity2(View view) {
-        TextView addressField = (TextView) findViewById(R.id.addressField);
-        TextView cityField = (TextView) findViewById(R.id.cityField);
-        Spinner stateField = (Spinner) findViewById(R.id.stateField);
-        String[] arr = {addressField.getText().toString(), cityField.getText().toString(), stateField.getSelectedItem().toString()};
+    public void setTextFields() {
+        TextView addr = (TextView)findViewById(R.id.addressField);
+        TextView city = (TextView)findViewById(R.id.cityField);
+        Spinner state = (Spinner)findViewById(R.id.stateField);
 
-        //  Intent intent = new Intent(MainActivity.this, Main3Activity.class);
-        //  startActivity(intent);
-        HTTPRequestClass httpRequest = new HTTPRequestClass();
-        httpRequest.execute(arr);
+        //****** HINT 1*****
+        String hint1 = "Street Address                            ";
+        String asterisk = "*";
+        SpannableStringBuilder builder1 = new SpannableStringBuilder();
+        builder1.append(hint1);
+        int start = builder1.length();
+        builder1.append(asterisk);
+        int end = builder1.length();
+        builder1.setSpan(new ForegroundColorSpan(RED), start, end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        addr.setHint(builder1);
+
+        //****** HINT 2*****
+        String hint2 = "City                                  ";
+        SpannableStringBuilder builder2 = new SpannableStringBuilder();
+        builder2.append(hint2);
+        int start2 = builder2.length();
+        builder2.append(asterisk);
+        int end2 = builder2.length();
+        builder2.setSpan(new ForegroundColorSpan(RED), start2, end2,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        city.setHint(builder2);
     }
 
+    public void goToActivity2(View view) {
+        TextView addressField = (TextView)findViewById(R.id.addressField);
+        TextView cityField = (TextView)findViewById(R.id.cityField);
+        Spinner stateField = (Spinner)findViewById(R.id.stateField);
+
+        if (addressField.getText().toString().matches("") || cityField.getText().toString().matches("") || stateField.getSelectedItemPosition()==0) {
+            alert("Please Fill All Fields");
+        }
+
+        else {
+            String[] arr = {addressField.getText().toString(), cityField.getText().toString(), stateField.getSelectedItem().toString()};
+
+            HTTPRequestClass httpRequest = new HTTPRequestClass();
+            httpRequest.execute(arr);
+
+        }
+    }
+
+    public void alert (String msg) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setMessage(msg);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        alertDialog.show();
+    }
 
     class HTTPRequestClass extends AsyncTask<String, Void, String> {
         //private final Context context;
